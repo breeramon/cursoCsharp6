9dd1b42e457c3607e016dd6f68c11a4684d58572
Breno Ramon <breeramon@gmail.com>
2023-05-19 10:14:49 -0300

    EXERCICIO - Criada uma classe funcionario e testadas usando listas

---
+++ Secao6/Funcionario.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Secao6
+{
+    class Funcionario
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Funcionario(int id, string name, double salary)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Salary = salary;
+        }
+
+        public void ExibirInfo()
+        {
+            Console.WriteLine("ID: {0}", Id);
+            Console.WriteLine("Name: {0}", Name);
+            Console.WriteLine("Salary: {0}", Salary);
+        }
+        
+        public void IncreaseSalary(double percentage)
+        {
+            double SalarioAumentado = this.Salary + (this.Salary * percentage / 100);
+            Console.WriteLine("Update Data: {0}, R$ {1}", this.Name, SalarioAumentado);
+        }
+
+        public override string ToString()
+        {
+            return Id + ", " + Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- Secao6/Program.cs
+++ Secao6/Program.cs
@@ -11,15 +11,48 @@ namespace Secao6
     {
         static void Main(string[] args)
         {
-            int s1 = Calculator.Soma(2, 3);
-            int s2 = Calculator.Soma(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-            int s3 = Calculator.Soma(20, 30, 40);
-            int s4 = Calculator.Soma(20, 30, 45, 55);
+            Console.Write("Digite o número de Funcionários para cadastrar: ");
+            int n = int.Parse(Console.ReadLine());
 
-            Console.WriteLine(s1);
-            Console.WriteLine(s2);
-            Console.WriteLine(s3);
-            Console.WriteLine(s4);
+            List<Funcionario> list = new List<Funcionario>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("Digite o Id do funcionario: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite o nome do funcionario: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Digite o salário do funcionário: ");
+                double salary = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+                list.Add(new Funcionario(id, name, salary));
+                Console.WriteLine("");
+            }
+
+            Console.Write("Digite o Id do funcionário que vai receber o aumento: ");
+            int searchId = int.Parse(Console.ReadLine());
+
+            Funcionario emp = list.Find(x => x.Id == searchId);
+            if (emp != null)
+            {
+                Console.Write("Valor da porcentagem: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.IncreaseSalary(percentage);
+            }
+            else
+            {
+                Console.WriteLine("Esse Id não existe!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of Funcionarios:");
+
+            foreach (Funcionario obj in list)
+            {
+                Console.WriteLine(obj);
+            }
 
             Console.ReadKey();
         }
