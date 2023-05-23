26c8f2b2d42838b610e5a51406092fbe75331dff
Breno Ramon <breeramon@gmail.com>
2023-05-23 09:10:06 -0300

    Enumerações, conversão para strings e organização de classes em folders

--- Secao6/Program.cs
+++ Secao6/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Globalization;
+using Secao6.Entities;
+using Secao6.Entities.Enums;
 
 namespace Secao6
 {
@@ -11,80 +13,26 @@ namespace Secao6
     {
         static void Main(string[] args)
         {
-            TimeSpan t1 = TimeSpan.FromDays(2); // pode ser em um dia quebrado como 1 dia e meio = 1.5
-            TimeSpan t2 = TimeSpan.FromHours(24); // tbm pode ser quebrado como 2 horas e meia = 2.5
-            TimeSpan t3 = TimeSpan.FromMinutes(1.5);
-            TimeSpan t4 = TimeSpan.FromSeconds(1.5);
-            TimeSpan t5 = TimeSpan.FromMilliseconds(1.5);
-            TimeSpan t6 = TimeSpan.FromTicks(900000000L);
-            
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
 
-            Console.WriteLine(t1);
-            Console.WriteLine(t2);
-            Console.WriteLine(t3);
-            Console.WriteLine(t4);
-            Console.WriteLine(t5);
-            Console.WriteLine(t6);
+            string txt = OrderStatus.PendingPayment.ToString();
 
-            DateTime d = new DateTime(2001, 8, 15, 13, 45, 58, 275);
-            Console.WriteLine(d);
-            Console.WriteLine("1) Date: " + d.Date);
-            Console.WriteLine("2) Day: " + d.Day);
-            Console.WriteLine("3) DayOfWeek: " + d.DayOfWeek);
-            Console.WriteLine("4) DayOfYear: " + d.DayOfYear);
-            Console.WriteLine("5) Hour: " + d.Hour);
-            Console.WriteLine("6) Kind: " + d.Kind);
-            Console.WriteLine("7) Millisecond: " + d.Millisecond);
-            Console.WriteLine("8) Minute: " + d.Minute);
-            Console.WriteLine("9) Month: " + d.Month);
-            Console.WriteLine("10) Second: " + d.Second);
-            Console.WriteLine("11) Ticks: " + d.Ticks);
-            Console.WriteLine("12) TimeOfDay: " + d.TimeOfDay);
-            Console.WriteLine("13) Year: " + d.Year);
+            //OrderStatus os1 = Enum.Parse<OrderStatus>("Delivered");
 
-            DateTime d1 = new DateTime(2020, 12, 21);
+            OrderStatus os = (OrderStatus)Enum.Parse(typeof(OrderStatus), "Delivered");
 
-            DateTime d2 = new DateTime(2020, 12, 23);
+            OrderStatus os2;
+            Enum.TryParse("Delivered", out os2);
 
-            DateTime y = d1.Add(t1); 
-            DateTime y1 = d1.AddDays(2);
-            DateTime y2 = d1.AddHours(2);
-            DateTime y3 = d1.AddMilliseconds(2);
-            DateTime y4 = d1.AddMinutes(2);
-            DateTime y5 = d1.AddMonths(2);
-            DateTime y6 = d1.AddSeconds(2);
-            DateTime y7 = d1.AddTicks(900000000L);
-            DateTime y8 = d1.AddYears(2);
-            DateTime y9 = d1.Subtract(t2);
-            TimeSpan t = d1.Subtract(d2);
-
-            TimeSpan t7 = new TimeSpan(2, 3, 5, 7, 11);
-            Console.WriteLine(t7);
-            Console.WriteLine("Days: " + t7.Days);
-            Console.WriteLine("Hours: " + t7.Hours);
-            Console.WriteLine("Minutes: " + t7.Minutes);
-            Console.WriteLine("Milliseconds: " + t7.Milliseconds);
-            Console.WriteLine("Seconds: " + t7.Seconds);
-            Console.WriteLine("Ticks: " + t7.Ticks);
-            Console.WriteLine("TotalDays: " + t7.TotalDays);
-            Console.WriteLine("TotalHours: " + t7.TotalHours);
-            Console.WriteLine("TotalMinutes: " + t7.TotalMinutes);
-            Console.WriteLine("TotalSeconds: " + t7.TotalSeconds);
-            Console.WriteLine("TotalMilliseconds: " + t7.TotalMilliseconds);
-
-            TimeSpan t8 = new TimeSpan(1, 30, 10);
-            TimeSpan t9 = new TimeSpan(0, 10, 5);
-            TimeSpan sum = t8.Add(t2);
-            TimeSpan dif = t8.Subtract(t2);
-            //TimeSpan mult = t9.Multiply(2.0);
-            //TimeSpan div = t9.Divide(2.0);
-
-            Console.WriteLine(t8);
-            Console.WriteLine(t9);
-            Console.WriteLine(sum);
-            Console.WriteLine(dif);
-            //Console.WriteLine(mult);
-            //Console.WriteLine(div);
+            Console.WriteLine(os2);
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
         }
     }
 }
